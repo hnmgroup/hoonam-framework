3329e7c7892512b53f53499c8053ee3b476727ff
Mehdi Eftekhari <me.golden.work@gmail.com>
2023-10-17 12:08:52 +0330

    migration base class issues fixed

--- src/Infrastructure/MigrationBase.php
+++ src/Infrastructure/MigrationBase.php
@@ -1,29 +1,18 @@
 <?php
 
-namespace Hoonam\Framework\Persistence;
+namespace Hoonam\Framework\Infrastructure;
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Schema\ColumnDefinition;
 
 abstract class MigrationBase extends Migration
 {
-    protected function id(Blueprint $table): void
-    {
-        $table->bigInteger('id')->autoIncrement();
-    }
-
     protected function createAuditColumns(Blueprint $table): void
     {
         $table->dateTime('created_at')->nullable(false);
         $table->bigInteger('created_by')->nullable(false);
         $table->dateTime('updated_at')->nullable(false);
         $table->bigInteger('updated_by')->nullable(false);
-    }
-
-    public function createMetaTagsColumn(Blueprint $table): ColumnDefinition
-    {
-        return $table->json('meta_tags')->nullable(false);
     }
 
     public function createTimeIntervalColumn(Blueprint $table, string $name, bool $nullable): void
